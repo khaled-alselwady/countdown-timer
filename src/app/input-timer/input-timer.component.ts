50d41419b2fafb4714c8452277f0a5e30b422422
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-10 23:16:24 +0300

    feat(input-timer): extract data using two-way binding and validate them

--- src/app/input-timer/input-timer.component.ts
+++ src/app/input-timer/input-timer.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTimer } from './input-timer.model';
 
 @Component({
   selector: 'app-input-timer',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './input-timer.component.html',
-  styleUrl: './input-timer.component.css'
+  styleUrl: './input-timer.component.css',
 })
 export class InputTimerComponent {
+  inputTimerData = signal<InputTimer>({ hours: 0, minutes: 0, title: '' });
 
+  validateInputs() {
+    if (this.inputTimerData().hours < 0) {
+      this.inputTimerData().hours = 0;
+    }
+    if (this.inputTimerData().minutes < 0) {
+      this.inputTimerData().minutes = 0;
+    }
+    if (this.inputTimerData().minutes > 60) {
+      this.inputTimerData().minutes = 60;
+    }
+    if (this.inputTimerData().title.trim() === '') {
+      this.inputTimerData().title = 'New Timer';
+    }
+  }
+
+  onSubmit() {
+    this.validateInputs();
+    console.log(this.inputTimerData());
+  }
 }
---
+++ src/app/input-timer/input-timer.model.ts
@@ -0,0 +1,5 @@
+export interface InputTimer {
+  hours: number;
+  minutes: number;
+  title: string;
+}
