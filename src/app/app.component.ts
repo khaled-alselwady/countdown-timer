72165749ccc9cb1ea969d7ff06320957ff642d02
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-10 22:56:40 +0300

    feat(input-timer): add input-timer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { InputTimerComponent } from "./input-timer/input-timer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, InputTimerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/input-timer/input-timer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-timer.component.html',
+  styleUrl: './input-timer.component.css'
+})
+export class InputTimerComponent {
+
+}
