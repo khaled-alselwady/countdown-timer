588649b32afa17490a7c0708f419b45430e2cd8e
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-10 22:17:12 +0300

    feat(header): add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'countdown-timer';
-}
+export class AppComponent {}
