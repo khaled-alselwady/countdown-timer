e377d0ca1aaa63ca821a069ed7b96a11f5a96c27
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-12 02:01:34 +0300

    refactor(timer): generalize timer component for reuse

---
+++ src/app/timers/timer/timer.component.ts
@@ -0,0 +1,95 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { TwoDigitPipe } from '../../shared/two-digit.pipe';
+import { Timer } from '../timer.model';
+
+@Component({
+  selector: 'tr[appTr]',
+  standalone: true,
+  imports: [TwoDigitPipe],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.css',
+})
+export class TimerComponent implements OnInit, OnDestroy {
+  @Input({ required: true }) timer!: Timer;
+  isPaused: boolean = false;
+  seconds = 59;
+  private intervalId?: ReturnType<typeof setInterval>;
+  @Output() delete = new EventEmitter<number>();
+
+  isTimeOver() {
+    return (
+      this.timer.hours <= 0 && this.timer.minutes <= 0 && this.seconds <= 0
+    );
+  }
+
+  handleCountdownTimer() {
+    if (!this.timer) {
+      return;
+    }
+
+    this.seconds--;
+
+    if (this.seconds < 0) {
+      this.seconds = 59;
+      this.timer.minutes--;
+    }
+
+    if (this.timer.minutes < 0) {
+      this.timer.minutes = 59;
+      this.timer.hours--;
+    }
+
+    if (this.timer.hours < 0) {
+      this.timer.hours = 0;
+    }
+    if (this.isTimeOver()) {
+      this.stopTimer();
+    }
+  }
+
+  startTimer() {
+    this.stopTimer();
+    this.intervalId = setInterval(() => this.handleCountdownTimer(), 500);
+  }
+
+  stopTimer() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+    }
+  }
+
+  validateTimer() {
+    if (this.timer.minutes < 0 && this.timer.hours !== 0) {
+      this.seconds = 0;
+    }
+  }
+
+  ngOnInit() {
+    this.validateTimer();
+    this.startTimer();
+  }
+
+  ngOnDestroy() {
+    this.stopTimer();
+  }
+
+  onPause() {
+    if (this.isPaused) {
+      this.startTimer();
+    } else {
+      this.stopTimer();
+    }
+    this.isPaused = !this.isPaused;
+  }
+
+  onDelete() {
+    this.delete.emit(this.timer.id);
+  }
+}
--- src/app/timers/timers.component.ts
+++ src/app/timers/timers.component.ts
@@ -1,35 +1,18 @@
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  QueryList,
-  Renderer2,
-  signal,
-  ViewChildren,
-  ViewEncapsulation,
-} from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import type { Timer } from './timer.model';
 import { InputTimerComponent } from './input-timer/input-timer.component';
 import { TwoDigitPipe } from '../shared/two-digit.pipe';
-import { CountdownTimerComponent } from './countdown-timer/countdown-timer.component';
+import { TimerComponent } from './timer/timer.component';
 
 @Component({
   selector: 'app-timers',
   standalone: true,
-  imports: [InputTimerComponent, TwoDigitPipe, CountdownTimerComponent],
+  imports: [InputTimerComponent, TwoDigitPipe, TimerComponent],
   templateUrl: './timers.component.html',
   styleUrl: './timers.component.css',
 })
 export class TimersComponent implements OnInit {
   timers = signal<Timer[]>([]);
-  isPaused = false;
-  @ViewChildren('pause') pauseButtons?: QueryList<
-    ElementRef<HTMLButtonElement>
-  >;
-  @ViewChildren(CountdownTimerComponent)
-  countdownTimerComponents?: QueryList<CountdownTimerComponent>;
-
-  constructor(private renderer: Renderer2) {}
 
   ngOnInit() {
     const timersJson = localStorage.getItem('timers');
@@ -55,69 +38,11 @@ export class TimersComponent implements OnInit {
     this.timers().push({
       id: this.createNewId(),
       hours: timerData.hours,
-      minutes: timerData.minutes,
+      minutes: --timerData.minutes,
       title: timerData.title,
     });
 
     this.saveToLocalStorage();
-  }
-
-  addClassesToPauseButton(button: HTMLButtonElement, isPaused: boolean) {
-    // Toggle classes using Renderer2
-    this.renderer.removeClass(button, isPaused ? 'pause' : 'resume');
-    this.renderer.addClass(button, isPaused ? 'resume' : 'pause');
-  }
-
-  changePauseButtonName(button: HTMLButtonElement) {
-    if (!button) {
-      return;
-    }
-
-    const isPaused = button.innerText === 'Pause';
-
-    button.innerText = isPaused ? 'Resume' : 'Pause';
-
-    this.addClassesToPauseButton(button, isPaused);
-  }
-
-  onTogglePause(id: number) {
-    if (!this.countdownTimerComponents) {
-      return;
-    }
-
-    for (const component of this.countdownTimerComponents) {
-      if (component.timerId === id) {
-        component.isPaused = !component.isPaused;
-        if (component.isPaused) {
-          component.stopTimer();
-        } else {
-          component.startTimer();
-        }
-        break;
-      }
-    }
-  }
-
-  onPause(id: number) {
-    if (!this.pauseButtons) {
-      return;
-    }
-
-    for (const buttonRef of this.pauseButtons) {
-      const button = buttonRef.nativeElement;
-      const timerId = button?.getAttribute('data-timer-id');
-      if (timerId === null) {
-        continue;
-      }
-
-      if (+timerId === id) {
-        this.changePauseButtonName(button);
-        break;
-      }
-    }
-
-    this.onTogglePause(id);
-    this.isPaused = !this.isPaused;
   }
 
   onDelete(id: number) {
